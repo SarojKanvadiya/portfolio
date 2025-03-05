8a1bf300c44483831857b747d823b5cb6a7860db
SarojKanvadiya <sarojkanvadiya@gmail.com>
2025-03-05 16:31:42 +0530

    change link

--- src/components/react/About.jsx
+++ src/components/react/About.jsx
@@ -1,5 +1,6 @@
 import { VStack, Text, Heading, Grid, GridItem,Button, Link, Center, Image, Box } from '@chakra-ui/react'
 import { FaFileArrowDown } from "react-icons/fa6";
+import Photo from "../../assets/sarojkanvadiya.jpg"
 import React from 'react'
 
 const About = () => {
@@ -11,14 +12,14 @@ const About = () => {
         <GridItem>
           
            <Center>
-           <img src='portfolio\src\assets\images\sarojkanvadiya.jpg' alt='sarojkanvadiya' w={{base:"100px", sm:"150px", md:"200px"}} border={"2px solid red"}/>
+           <img src='portfolio/src/assets/images/sarojkanvadiya.jpg' alt='sarojkanvadiya' w={{base:"100px", sm:"150px", md:"200px"}} border={"2px solid red"}/>
            </Center>
            
         </GridItem>
         <GridItem>
             <Text p={{base:0, sm:2}} fontSize={{base:"14px", sm:"15px"}}>Dynamic and detail-oriented Front-End Developer skilled in building responsive user interfaces using HTML, CSS, JavaScript, and React. Proficient in leveraging the DOM for interactive designs and experienced with Git for version control. A quick learner committed to delivering clean and efficient code.</Text>
             <Center>
-            <Link href='src\assets\SarojKanvadiya-FrontendDeveloper(React)-WWwB.pdf' download={'SarojKanvadiya-FrontendDeveloper(React)'}>
+            <Link href='https://ibb.co/7tGDKZSD' download={'SarojKanvadiya-FrontendDeveloper(React)'}>
             <Button size={"sm"} colorPalette={"teal"} m={3}><FaFileArrowDown /> Resume</Button>
             </Link>
             </Center>
